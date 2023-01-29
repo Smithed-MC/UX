7fdfe441e4609ae977f1a564c428e8951b9ed7d9
TheNuclearNexus <thenuclearnexus@gmail.com>
2023-01-29 13:47:11 -0500

    fix: removed cors from v2

--- platforms/api/src/app.ts
+++ platforms/api/src/app.ts
@@ -8,24 +8,30 @@ export const API_APP = fastify().withTypeProvider<TypeBoxTypeProvider>();
 
 export async function importRoutes(dirPath: string) {
     let files = fs.readdirSync('src/' + dirPath)
-  
+
     for (let file of files) {
-      if (fs.statSync('src/' + dirPath + "/" + file).isDirectory()) {
-        await importRoutes(dirPath + "/" + file)
-      } else {
-        const parts = file.split('.')
-  
-        const ext = parts[parts.length - 1]
-        console.log("./" + dirPath + "/" + file)
-        if (ext === 'js' || ext === 'ts')
-          await import("./" + dirPath + "/" + file)
-      }
+        if (fs.statSync('src/' + dirPath + "/" + file).isDirectory()) {
+            await importRoutes(dirPath + "/" + file)
+        } else {
+            const parts = file.split('.')
+
+            const ext = parts[parts.length - 1]
+            console.log("./" + dirPath + "/" + file)
+            if (ext === 'js' || ext === 'ts')
+                await import("./" + dirPath + "/" + file)
+        }
     }
-  }
-  
+}
+
 export async function setupApp() {
     await initialize()
     await importRoutes('routes')
+
+    API_APP.addHook("onResponse", (response) => {
+        response.headers['Access-Control-Allow-Origin'] = '*';
+        response.headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept'
+        response.headers['Access-Control-Allow-Methods'] = '*'
+    })
 
     return API_APP;
 }
