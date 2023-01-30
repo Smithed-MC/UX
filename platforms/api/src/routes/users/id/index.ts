00326c2fe32057a5647602544c60d3141a0d6480
TheNuclearNexus <thenuclearnexus@gmail.com>
2023-01-30 10:11:08 -0500

    feat: /users/:id & /users/:id/packs

---
+++ platforms/api/src/routes/users/id/index.ts
@@ -0,0 +1,41 @@
+import { Type } from '@sinclair/typebox'
+import { API_APP } from '../../../app.js'
+import { getFirestore } from 'firebase-admin/firestore'
+import { sanitize } from '../../sanitize.js'
+import { HTTPResponses } from 'data-types'
+
+export async function getUserDoc(id: string) {
+
+    const firestore = getFirestore()
+    const packs = firestore.collection('users')
+
+    const doc = await packs.doc(id).get()
+    if (doc.exists) {
+        return doc
+    }
+    const query = await packs.where('cleanName', '==', sanitize(id)).limit(1).get()
+
+    if (query.docs.length == 0)
+        return undefined
+
+    return query.docs[0]
+}
+
+API_APP.route({
+    method: 'GET',
+    url: '/users/:id',
+    schema: {
+        params: Type.Object({
+            id: Type.String()
+        })
+    },
+    handler: async (request, reply) => {
+        const {id} = request.params
+
+        const userDoc = await getUserDoc(id)
+
+        if(userDoc === undefined)
+            return reply.status(HTTPResponses.NOT_FOUND).send('User not found')
+        return userDoc.data()
+    }
+})
---
+++ platforms/api/src/routes/users/id/packs.ts
@@ -0,0 +1,29 @@
+import { Type } from "@sinclair/typebox";
+import { API_APP } from "../../../app.js";
+import { getUserDoc } from "./index.js";
+import { HTTPResponses } from "data-types";
+import { getFirestore } from "firebase-admin/firestore";
+
+
+API_APP.route({
+    method: 'GET',
+    url: '/users/:id/packs',
+    schema: {
+        params: Type.Object({
+            id: Type.String()
+        })
+    },
+    handler: async (request, reply) => {
+        const {id} = request.params
+
+        const userDoc = await getUserDoc(id)
+        if(userDoc === undefined)
+            return reply.status(HTTPResponses.NOT_FOUND).send('User not found')
+        
+        const packs = getFirestore().collection('packs')
+
+        const query = await packs.where('contributors', 'array-contains', userDoc.id).get()
+        
+        return query.docs.map(d => d.id)
+    }
+})
