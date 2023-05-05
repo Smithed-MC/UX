562757cc31baa259f38547bc9f85b133fae74994
TheNuclearNexus <thenuclearnexus@gmail.com>
2023-05-05 18:25:49 -0400

    feat: download button has spinner

--- packages/components/DownloadButton.tsx
+++ packages/components/DownloadButton.tsx
@@ -1,28 +1,40 @@
 import React, { useState } from 'react'
 import { Download } from './svg'
 import saveAs from 'file-saver'
+import Spinner from './Spinner'
 interface DownloadButtonProps {
     link: string
 }
 
+
 export default function DownloadButton({ link }: DownloadButtonProps) {
     const [downloading, setDownloading] = useState(false)
     return <div style={{ width: 48, height: 48, flexShrink: 0 }}>
-        <a className="button wobbleHover container" style={{ maxWidth: 48, maxHeight: 48, borderRadius: '24px', padding: 12 }} href={link} onClick={async (e) => {
+        <a className="button wobbleHover container" style={{ width: 48, height: 48, borderRadius: '24px', padding: 12, boxSizing: 'border-box', overflow: 'clip', justifyContent: 'center' }} href={link} onClick={async (e) => {
             e.preventDefault()
-            setDownloading(true)
             
+            setDownloading(true)
+
             const resp = await fetch(link)
-            for(let h of resp.headers) {
-                console.log(h)
+            if (!resp.ok) {
+                setDownloading(false)
+                return
             }
-            saveAs(await resp.blob())
+                
+            
+            const disposition = resp.headers.get('content-disposition')
+            if (disposition == null) {
+                setDownloading(false)
+                return
+            }
+
+            const filename = disposition.matchAll(/.*filename="(.+)"/g).next().value[1]
+
+            saveAs(await resp.blob(), filename)
             setDownloading(false)
         }}>
-            <Download style={{ fill: 'var(--buttonText)' }} />
+            {!downloading && <Download style={{ fill: 'var(--buttonText)' }} />}
+            { downloading && <Spinner style={{width: 32, height: 32, position: 'absolute', border: '4px solid var(--subText)', borderTop:'4px solid var(--buttonText)'}}/>}
         </a>
-        {downloading && <div style={{position: 'fixed', top: 0, left: 0, width: '100%', height: '100%', backgroundColor: 'rgba(0,0,0,0.5)'}}>
-
-        </div>}
     </div>
 }
