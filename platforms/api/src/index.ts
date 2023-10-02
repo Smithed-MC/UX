d6c56746e5d1fc6c848d71e47caa913bfc1c1e4c
TheNuclearNexus <39636175+TheNuclearNexus@users.noreply.github.com>
2023-10-02 18:03:39 -0400

    feat: more docker changes

--- platforms/api/src/index.ts
+++ platforms/api/src/index.ts
@@ -28,7 +28,7 @@ async function listen(port: number) {
   console.log('Starting to listen on ', port)
 
   await setupApp()
-  await API_APP.listen({ port: port, host: '127.0.0.1' })
+  await API_APP.listen({ port: port, host: process.env.DOCKER ? '0.0.0.0' : '127.0.0.1' })
 
   executeTasks()
   setInterval(executeTasks, 60 * 1000)
