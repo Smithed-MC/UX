3b1f09013bd34719092c53122843be00ed3ab5de
TheNuclearNexus <39636175+TheNuclearNexus@users.noreply.github.com>
2023-11-30 12:07:44 -0500

    bump: 1.20.3-rc1

--- packages/data-types/index.ts
+++ packages/data-types/index.ts
@@ -13,6 +13,7 @@ Format.Set('semver', (v) => coerce(v) != null)
 console.log(Format.Has('semver'))
 
 export const supportedMinecraftVersions = [
+    '1.20.3-rc1',
     '1.20.3-pre4',
     '1.20.2',
     '1.20.1',
