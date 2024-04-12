ebc79b25ffa9aaa4733209efdf0d24353936f43a
TheNuclearNexus <39636175+TheNuclearNexus@users.noreply.github.com>
2024-04-12 09:53:48 -0400

    fix: begin updating launcher to new UI changes

--- platforms/desktop/src/main.tsx
+++ platforms/desktop/src/main.tsx
@@ -30,108 +30,101 @@ if (!subRoutes.includes(editLocalBundleRoute)) {
 	subRoutes.push(editLocalBundleRoute)
 }
 
-let inject: ClientInject = {
-	getNavbarTabs: () => {
-		return [
-			<IconTextButton
-				className="navBarOption start"
-				text="Launch"
-				href="/launch"
-				iconElement={<svg.Play fill="white" />}
-			/>,
-			<IconTextButton
-				className="navBarOption middle"
-				text="Browse"
-				href="/browse"
-				icon={svg.Browse}
-			/>,
-			<IconTextButton
-				className="navBarOption middle"
-				text="Discord"
-				href="https://smithed.dev/discord"
-				target="_blank"
-				icon={svg.Discord}
-			/>,
-		]
-	},
-	enableFooter: false,
-	logoUrl: "/launch",
-	packDownloadButton: (id, openPopup, closePopup) => (
-		<IconTextButton
-			className="accentedButtonLike"
-			iconElement={<svg.Plus fill="var(--foreground)" />}
-			text={"Add to local bundle"}
-			onClick={() => {
-				const element = (
-					<AddToBundle
-						packId={id}
-						onFinish={async (bundleId, packVersion) => {
-							if (
-								bundleId !== undefined &&
-								packVersion !== undefined
-							) {
-								const ref: PackReference = {
-									id: id,
-									version: packVersion,
-								}
-								try {
-									await invoke("add_pack_to_bundle", {
-										bundleId: bundleId,
-										pack: ref,
-									})
-								} catch (e) {
-									console.error(
-										"Failed to add pack to bundle: " + e
-									)
-								}
-							}
-							closePopup()
-						}}
-					/>
-				)
-				openPopup(element)
-			}}
-			reverse
-		/>
-	),
-	bundleDownloadButton: (id, openPopup, closePopup) => (
-		<IconTextButton
-			className="accentedButtonLike"
-			iconElement={<svg.Download fill="var(--foreground)" />}
-			text={"Import"}
-			onClick={() => {
-				const element = (
-					<ImportBundle
-						bundleId={id}
-						onFinish={async (bundleId, bundle) => {
-							if (
-								bundleId !== undefined &&
-								bundle !== undefined
-							) {
-								try {
-									await invoke("add_bundle", {
-										bundleId: bundleId,
-										bundle: bundle,
-									})
-								} catch (e) {
-									console.error(
-										"Failed to add imported bundle: " + e
-									)
-								}
-							}
-							closePopup()
-						}}
-					/>
-				)
-				openPopup(element)
-			}}
-			reverse
-		/>
-	),
-	showBackButton: true,
-}
+// let inject: ClientInject = {
+// 	getNavbarTabs: () => {
+// 		return [
+// 			<IconTextButton
+// 				className="navBarOption start"
+// 				text="Launch"
+// 				href="/launch"
+// 				iconElement={<svg.Play fill="white" />}
+// 			/>,
+// 			<IconTextButton
+// 				className="navBarOption middle"
+// 				text="Packs"
+// 				href="/packs"
+// 				icon={svg.Browse}
+// 			/>
+// 		]
+// 	},
+// 	enableFooter: false,
+// 	logoUrl: "/launch",
+// 	// packDownloadButton: (id, openPopup, closePopup) => (
+// 	// 	<IconTextButton
+// 	// 		className="accentedButtonLike"
+// 	// 		iconElement={<svg.Plus fill="var(--foreground)" />}
+// 	// 		text={"Add to local bundle"}
+// 	// 		onClick={() => {
+// 	// 			const element = (
+// 	// 				<AddToBundle
+// 	// 					packId={id}
+// 	// 					onFinish={async (bundleId, packVersion) => {
+// 	// 						if (
+// 	// 							bundleId !== undefined &&
+// 	// 							packVersion !== undefined
+// 	// 						) {
+// 	// 							const ref: PackReference = {
+// 	// 								id: id,
+// 	// 								version: packVersion,
+// 	// 							}
+// 	// 							try {
+// 	// 								await invoke("add_pack_to_bundle", {
+// 	// 									bundleId: bundleId,
+// 	// 									pack: ref,
+// 	// 								})
+// 	// 							} catch (e) {
+// 	// 								console.error(
+// 	// 									"Failed to add pack to bundle: " + e
+// 	// 								)
+// 	// 							}
+// 	// 						}
+// 	// 						closePopup()
+// 	// 					}}
+// 	// 				/>
+// 	// 			)
+// 	// 			openPopup(element)
+// 	// 		}}
+// 	// 		reverse
+// 	// 	/>
+// 	// ),
+// 	// bundleDownloadButton: (id, openPopup, closePopup) => (
+// 	// 	<IconTextButton
+// 	// 		className="accentedButtonLike"
+// 	// 		iconElement={<svg.Download fill="var(--foreground)" />}
+// 	// 		text={"Import"}
+// 	// 		onClick={() => {
+// 	// 			const element = (
+// 	// 				<ImportBundle
+// 	// 					bundleId={id}
+// 	// 					onFinish={async (bundleId, bundle) => {
+// 	// 						if (
+// 	// 							bundleId !== undefined &&
+// 	// 							bundle !== undefined
+// 	// 						) {
+// 	// 							try {
+// 	// 								await invoke("add_bundle", {
+// 	// 									bundleId: bundleId,
+// 	// 									bundle: bundle,
+// 	// 								})
+// 	// 							} catch (e) {
+// 	// 								console.error(
+// 	// 									"Failed to add imported bundle: " + e
+// 	// 								)
+// 	// 							}
+// 	// 						}
+// 	// 						closePopup()
+// 	// 					}}
+// 	// 				/>
+// 	// 			)
+// 	// 			openPopup(element)
+// 	// 		}}
+// 	// 		reverse
+// 	// 	/>
+// 	// ),
+// 	showBackButton: true,
+// }
 
-populateRouteProps({ platform: "desktop", inject: inject })
+// populateRouteProps({ platform: "desktop",   })
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
