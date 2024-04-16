39513919a9e58a0f9e26b30857f97139107ff8ce
TheNuclearNexus <39636175+TheNuclearNexus@users.noreply.github.com>
2024-04-16 16:46:50 -0400

    fix: doc workflow

---
+++ packages/doc-generator/index.js
@@ -0,0 +1,211 @@
+import * as fs from "fs";
+import * as path from "path";
+import { BundleSchema, HTTPResponses, MetaDataSchema, MinecraftVersionSchema, PackCategorySchema, PackDataSchema, PackReferenceSchema, PackVersionSchema, PATokenSchema, SortSchema, UserDataSchema, } from "data-types";
+import { Kind, Modifier, } from "@sinclair/typebox";
+const defaultVariableTemplate = (groups) => {
+    let type = groups[1];
+    const typeMatch = type.matchAll(/([^\[\?]+)(.*)+/g).next();
+    if (!["string", "number", "boolean"].includes(typeMatch.value[1])) {
+        const schema = registeredSchemas.find((r) => r.name === typeMatch.value[1]);
+        console.log(schema);
+        if (schema !== undefined)
+            type = `[${typeMatch.value[1]}](/api/data-types)${typeMatch.value[2]}`;
+    }
+    return `{bdg-dark}\`${groups[0]}\` <label class="sd-text-secondary">${type}</label>${groups[2]}\n`;
+};
+const HDIV = `\n<div class='sd-bg-secondary' style='width: 95%; height: 1px; margin: 0em 0em 0.1em 0em'></div>\n`;
+const POSSIBLE_DIRECTIVES = {
+    route: {
+        pattern: /@route (.+) (.+)((\s*.*)+)/g,
+        template: (groups) => `### {bdg-success}\`${groups[0]}\`\n${groups[2]}`,
+    },
+    query: {
+        pattern: /@query ([^:]+):\s*(.+)((\s*.*)+)/g,
+        template: defaultVariableTemplate,
+    },
+    return: {
+        pattern: /@return ([^:]+):\s*(.+)/g,
+        template: (groups) => `{bdg-primary}\`${groups[0]} - ${HTTPResponses[groups[0]]}\` <label class="sd-text-secondary">${groups[1]}</label>\n`,
+    },
+    example: {
+        pattern: /@example (.+)((\s.*)+)/g,
+        template: (groups) => `::::{admonition} ${groups[0]}
+    :class: note        
+\`\`\`ts${groups[1]}
+\`\`\`
+::::\n`,
+    },
+    param: {
+        pattern: /@param (.+)((\s*.*)+)/g,
+        template: (groups) => `**${groups[0]}**:${groups[1]}`,
+    },
+    body: {
+        pattern: /@body ([^:]+):\s*(.+)((\s*.*)+)/g,
+        template: defaultVariableTemplate,
+    },
+};
+const COMMENT_BLOCK = /\/\*(\s*\*.*)*\s+\*\//g;
+const DIRECTIVE = /(@\w+)(.+)?(\s(?!@\w).+)*/g;
+function parseDirective(content) {
+    for (const name in POSSIBLE_DIRECTIVES) {
+        const d = POSSIBLE_DIRECTIVES[name];
+        const match = content
+            .matchAll(d.pattern instanceof RegExp
+            ? d.pattern
+            : new RegExp(d.pattern, "g"))
+            .next();
+        if (match.done)
+            continue;
+        const groups = match.value.slice(1);
+        return [name, groups];
+    }
+    return ["none", [content]];
+}
+const files = {};
+function generateDirectives(type, directives, header) {
+    if (directives[type] === undefined || directives[type].length === 0)
+        return "";
+    return ((header !== undefined ? header + "\n" : "") +
+        directives[type]
+            .map((groups) => POSSIBLE_DIRECTIVES[type].template(groups))
+            .join("\n"));
+}
+function extract(t, config) {
+    const content = fs.readFileSync(t, { encoding: "utf-8" });
+    const blocks = content.matchAll(COMMENT_BLOCK);
+    for (let [text, ...other] of blocks) {
+        text = text.replace(/\s*(\/)?\*(\/)? ?/g, "\n");
+        const directives = text.matchAll(DIRECTIVE);
+        const parsedDirectives = {};
+        for (const [directiveContent, ...other] of directives) {
+            const d = parseDirective(directiveContent);
+            if (parsedDirectives[d[0]] === undefined)
+                parsedDirectives[d[0]] = [];
+            parsedDirectives[d[0]].push(d[1]);
+        }
+        let extractedDoc = [
+            generateDirectives("route", parsedDirectives),
+            generateDirectives("param", parsedDirectives, "\n\n#### URL Parameters" + HDIV),
+            generateDirectives("query", parsedDirectives, "\n\n#### Query Parameters" + HDIV),
+            generateDirectives("body", parsedDirectives, "\n\n#### Body Parameters" + HDIV),
+            generateDirectives("return", parsedDirectives, "\n\n#### Possible Responses" + HDIV),
+            generateDirectives("example", parsedDirectives, "\n\n#### Examples" + HDIV),
+            "<br/>\n\n",
+        ].join("\n") + "\n";
+        if (parsedDirectives["route"] === undefined ||
+            parsedDirectives["route"].length === 0)
+            continue;
+        const routeGroup = parsedDirectives["route"][0];
+        const route = routeGroup[1];
+        const filePath = path.join(config.docRepo, route.replaceAll(":", "") + ".inc");
+        if (files[filePath] === undefined)
+            files[filePath] = "## " + route + "\n" + extractedDoc;
+        else
+            files[filePath] += extractedDoc;
+    }
+}
+function readDirectory(t, config) {
+    if (!fs.existsSync(t)) {
+        console.log(`${t} does not exist`);
+        return;
+    }
+    const contents = fs.readdirSync(t, { withFileTypes: true });
+    for (const c of contents) {
+        if (c.isDirectory())
+            readDirectory(path.join(t, c.name), config);
+        if (c.isFile() && c.name.matchAll(config.extensions))
+            extract(path.join(t, c.name), config);
+    }
+}
+function propertyToNiceFormat(key, property, level) {
+    let indent = " ".repeat(level * 2);
+    let structure = key != "" ? `${indent}${key}: ` : "";
+    const registeredSchema = registeredSchemas.find((r) => r.value === property);
+    if (registeredSchema !== undefined) {
+        structure += `${registeredSchema.name}`;
+    }
+    else if (property["type"] == "object") {
+        structure += "{\n";
+        for (const key of Object.keys(property.properties)) {
+            const value = property.properties[key];
+            structure += propertyToNiceFormat(key, value, level + 1) + "\n";
+        }
+        structure += indent + "}";
+    }
+    else if (property["type"] == "array") {
+        structure += `${propertyToNiceFormat("", property.items, level)}[]`;
+    }
+    else if (property[Kind] === "Union") {
+        structure += property.anyOf
+            .map((p) => propertyToNiceFormat("", p, level))
+            .join(" | ");
+    }
+    else if (property[Kind] === "Literal") {
+        const value = property.const;
+        if (typeof value.valueOf() === "string")
+            structure += "'" + value.valueOf() + "'";
+        else
+            structure += value.valueOf();
+    }
+    else {
+        structure += `${property["type"]}`;
+    }
+    if (property[Modifier] === "Optional")
+        structure += "?";
+    return structure;
+}
+const registeredSchemas = [];
+function schemaToNiceFormat(schema, level = 1) {
+    return propertyToNiceFormat("", schema, level);
+}
+function registerSchema(name, schema) {
+    const prettified = `### ${name}\n\`\`\`ts\n${schemaToNiceFormat(schema, 0)}\n\`\`\``;
+    registeredSchemas.push({ name: name, value: schema });
+    return prettified;
+}
+function start(config) {
+    if (fs.existsSync(config.docRepo))
+        fs.rmSync(config.docRepo, { recursive: true, force: true });
+    fs.mkdirSync(config.docRepo);
+    const dataTypes = [
+        registerSchema("PackReference", PackReferenceSchema),
+        registerSchema("PackCategory", PackCategorySchema),
+        registerSchema("MinecraftVersion", MinecraftVersionSchema),
+        registerSchema("PackVersion", PackVersionSchema),
+        registerSchema("PackData", PackDataSchema),
+        registerSchema("PackMetaData", MetaDataSchema),
+        registerSchema("PackBundle", BundleSchema),
+        registerSchema("UserData", UserDataSchema),
+        registerSchema("SortOptions", SortSchema),
+        registerSchema("PAToken", PATokenSchema)
+    ].join("\n");
+    fs.writeFileSync(path.join(config.docRepo, "data-types.inc"), dataTypes);
+    for (const t of config.targets) {
+        readDirectory(t, config);
+    }
+    const imports = [];
+    for (const file in files) {
+        console.log(file);
+        file.split(path.sep).forEach((cur, idx, arr) => {
+            if (idx === arr.length - 1)
+                return;
+            const part = arr.slice(0, idx + 1).join(path.sep);
+            if (!fs.existsSync(part) && part !== "")
+                fs.mkdirSync(part);
+        });
+        fs.writeFileSync(file, files[file]);
+        imports.push(path
+            .join(path.basename(config.docRepo), file.replace(path.normalize(config.docRepo), ""))
+            .replaceAll(path.sep, "/"));
+    }
+    fs.writeFileSync(path.join(config.docRepo, "routes.inc"), imports
+        .map((i) => `
+\`\`\`{include} ${i}
+\`\`\``)
+        .join("\n"));
+}
+if (fs.existsSync("./config.json")) {
+    const config = JSON.parse(fs.readFileSync("./config.json", { encoding: "utf-8" }));
+    config.docRepo = path.join(process.env.DOC_REPO ?? config.docRepo, "docs/api/generated");
+    start(config);
+}
